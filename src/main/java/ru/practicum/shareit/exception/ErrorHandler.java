70d121cf65295662bc45cfe3fa557951310ea48a
kriulkin <a.kriulkin@gmail.com>
2023-05-08 02:36:43 +1000

    checkstyle: вынес ErrorResponse в отдельный файл

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -40,14 +40,3 @@ public class ErrorHandler {
     }
 }
 
-class ErrorResponse {
-    private final String error;
-
-    public ErrorResponse(String error) {
-        this.error = error;
-    }
-
-    public String getError() {
-        return error;
-    }
-}
